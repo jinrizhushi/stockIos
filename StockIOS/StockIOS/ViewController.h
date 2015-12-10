7c681b7e6c9ffa452e5d27aa1df98c6fff9f05fc
bamboo <jinrizhushi>
2015-12-10 23:36:55 +0800

    Add:add the project framework

---
+++ StockIOS/StockIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StockIOS
+//
+//  Created by bamboo on 15/12/10.
+//  Copyright © 2015年 bamboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StockIOS/StockIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StockIOS
+//
+//  Created by bamboo on 15/12/10.
+//  Copyright © 2015年 bamboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
